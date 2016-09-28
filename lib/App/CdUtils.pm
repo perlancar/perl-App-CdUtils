cf23411a4176536c59938905a78048f87c35cc62
perlancar (@pc-office) <perlancar@gmail.com>
2016-09-28 14:00:48 +0700

    Initial commit

---
+++ lib/App/CdUtils.pm
@@ -0,0 +1,22 @@
+package App::CdUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+#our %SPEC;
+
+1;
+
+# ABSTRACT: CLI utilities related to changing directories
+
+=head1 DESCRIPTION
+
+This distribution contains the following utilities:
+
+# INSERT_EXECS_LISTS
+
+=cut
